b7b6689993d51c34794cea7cec21f3b155c8f8ff
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-24 22:51:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [a, b, c, d]
+  ["a, b, c, d]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift = element
+  array = element.unshift
 end
 
 def remove_element_from_end_of_array(array)
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[1]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
